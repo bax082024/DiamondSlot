b4cc89dd41e7b5006ca16c50928b804b73a47e12
bax082024 <bax082024@gmail.com>
2024-12-24 09:35:33 +0100

    first commit

--- obj/Debug/net8.0-windows/Slot777.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Slot777.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Slot777")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d23715aea3e619f78a57e39a88420d316158c41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dda6763d88fd76e801efd2a61df1f31cfe4779a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Slot777")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Slot777")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
